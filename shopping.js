450623e711ae261692892608c9f4ebd2e39c8d26
Mohammad Akbar <mohammad.akbar2603@gmail.com>
2021-02-03 18:53:02 +0600

    Added totalprice row, work left on localstorage

--- shopping.js
+++ shopping.js
@@ -1,64 +1,73 @@
 //Get the ui elements
 let cart = document.querySelectorAll('input');
 let cartlist = document.querySelector('#itemlist');
+const totalprice = document.querySelector('#total');
+//console.log(parseInt(totalprice.innerHTML));
 
 
 
 //Item Class
-class Item{
-    constructor(name,price){
-        this.name=name;
-        this.price=price;
-        
+class Item {
+    constructor(name, price, quantity, total) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.total = total;
     }
 }
 
 //UI class
-class UI{
-    static addItemList(item){
+class UI {
+    static addItemList(item) {
         let list = document.querySelector('#itemlist');
         const row = document.createElement('tr');
-        
-        for(let i=0;i<list.children.length;i++){
-            if(list.children[i].children[0].textContent.toString() == item.name.toString()){
+
+
+        for (let i = 0; i < list.children.length; i++) {
+            if (list.children[i].children[0].textContent.toString() == item.name.toString()) {
                 list.children[i].remove();
             }
         }
-        
+
         //list.innerHTML='';
-        
+
         row.innerHTML = `<td>${item.name}</td>
-        <td>${item.price}</td>
+        <td>${item.quantity}</td>
+        <td>${item.price * item.quantity}</td>
         <td><a href='#' class='delete'>X</a></td>`;
         list.appendChild(row);
+        totalprice.innerHTML = item.total;
         //console.log(list.children[0]);
-        
+
     }
-    
-    showAlert(message,className){
+
+    showAlert(message, className) {
         this.clearAlert();
-        
+
         let div = document.createElement('div');
         div.className = `alert ${className}`;
         div.appendChild(document.createTextNode(message));
         let container = document.querySelector('.container');
-        let table  =document.querySelector('#item_table');
-        container.insertBefore(div,table);
+        let table = document.querySelector('#item_table');
+        container.insertBefore(div, table);
 
-        setTimeout( () => {
+        setTimeout(() => {
             document.querySelector('.alert').remove();
-        },3000);
+        }, 500);
     }
 
-    clearAlert(){
+    clearAlert() {
         let currentAlert = document.querySelector('.alert')
-        if(currentAlert){
+        if (currentAlert) {
             currentAlert.remove();
         }
     }
 
-    removefromcart(target){
-        if(target.hasAttribute('href')){
+    removefromcart(target) {
+
+        if (target.hasAttribute('href')) {
+            totalprice.innerHTML = (parseInt(totalprice.textContent) - parseInt(target.parentElement.previousElementSibling.textContent)).toString();
+
             target.parentElement.parentElement.remove();
             Store.removeitemfromlocalstorage(target.parentElement.parentElement.firstChild.textContent.trim());
             this.showAlert("Book Removed!", 'success')
@@ -67,53 +76,59 @@ class UI{
 
 }
 
-class Store{
-    static getItems(){
+class Store {
+    static getItems() {
         let items;
-        if(localStorage.getItem('items')===null){
-            items=[]
+        if (localStorage.getItem('items') === null) {
+            items = []
         }
-        else{
+        else {
             items = JSON.parse(localStorage.getItem('items'));
         }
         return items;
     }
 
-    static addItem(item){
+    static addItem(item) {
         let items = Store.getItems();
         items.push(item);
         localStorage.setItem('items', JSON.stringify(items));
     }
 
-    static displayItems(){
+    static displayItems() {
         let items = Store.getItems();
-        items.forEach(item =>{
+        items.forEach(item => {
             UI.addItemList(item);
         })
     }
 
-    static removeitemfromlocalstorage(name){
+    static removeitemfromlocalstorage(name) {
         let items = Store.getItems();
-        items.forEach((item,index) =>{
-            if(item.name===name){
-                items.splice(index,1);
+        items.forEach((item, index) => {
+            if (item.name === name) {
+                items.splice(index, 1);
             }
         })
-        localStorage.setItem('items',JSON.stringify(items));
-}
+        localStorage.setItem('items', JSON.stringify(items));
+    }
 }
 //Add event listeners
 cartlist.addEventListener('click', removeitem);
 document.addEventListener('DOMContentLoaded', Store.displayItems());
 
 //Defining Functions
-for(let i=0;i<cart.length;i++){
+for (let i = 0; i < cart.length; i++) {
     let button = cart[i];
-    button.addEventListener('click', function(e){
-        let product_name= e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
-        let product_price= e.target.parentElement.previousElementSibling.textContent;
-        
-        let item = new Item(product_name,product_price);
+    let count = 0;
+    var total = 0;
+    button.addEventListener('click', function (e) {
+        let product_name = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+        let product_price = e.target.parentElement.previousElementSibling.textContent;
+        count += 1;
+        total += parseInt(product_price);
+        console.log(total);
+        //totalprice.innerHTML = total;
+        product_quantity = count;
+        let item = new Item(product_name, product_price, product_quantity, total);
         UI.addItemList(item);
         let ui = new UI();
         ui.showAlert("Book Added!", 'success');
@@ -123,8 +138,8 @@ for(let i=0;i<cart.length;i++){
     })
 }
 
-function removeitem(e){
-    let ui = new UI()
+function removeitem(e) {
+    let ui = new UI();
     ui.removefromcart(e.target);
     e.preventDefault();
 
